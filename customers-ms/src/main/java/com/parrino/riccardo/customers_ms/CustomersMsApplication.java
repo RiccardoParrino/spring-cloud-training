509f2147721d9e5d51ab822c33d1ee779f2f82df
RiccardoParrino <riccardoparrino1@gmail.com>
2025-11-13 07:37:33 +0100

    add three ms

---
+++ customers-ms/src/main/java/com/parrino/riccardo/customers_ms/CustomersMsApplication.java
@@ -0,0 +1,13 @@
+package com.parrino.riccardo.customers_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersMsApplication.class, args);
+	}
+
+}
---
+++ customers-ms/src/test/java/com/parrino/riccardo/customers_ms/CustomersMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parrino.riccardo.customers_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
