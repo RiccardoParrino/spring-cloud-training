d5d0beb2d90ad6626b1def183df1a12db2989223
RiccardoParrino <riccardoparrino1@gmail.com>
2025-11-14 16:10:07 +0100

    fixing migration to webClient

--- sales-ms/src/main/java/com/parrino/riccardo/sales_ms/webClient/ProductsWebClient.java
+++ sales-ms/src/main/java/com/parrino/riccardo/sales_ms/webClient/ProductsWebClient.java
@@ -1,6 +1,5 @@
 package com.parrino.riccardo.sales_ms.webClient;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -9,8 +8,11 @@ import reactor.core.publisher.Mono;
 @Service
 public class ProductsWebClient {
 
-    @Autowired
-    private WebClient webClient;
+    private final WebClient webClient;
+
+    public ProductsWebClient(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
 
     public Mono<String> findProductByCode(Long code) {
         return webClient.get()
